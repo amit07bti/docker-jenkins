670ea3b341b6e241cf44bb5cecd54cbed637e74d
amit07bti <amitkumar.developer764@gmail.com>
2025-07-29 13:41:43 +0530

    first commit

---
+++ docker-jenkins/src/test/java/com/edusol/DockerJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edusol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
