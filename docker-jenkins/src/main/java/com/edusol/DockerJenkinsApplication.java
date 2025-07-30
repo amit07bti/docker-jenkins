f18ad05f0f57ad6391e4da6b2932c6b30f5ba010
amit07bti <amitkumar.developer764@gmail.com>
2025-07-30 14:03:59 +0530

    agian change again thdsdscssdcd

--- docker-jenkins/src/main/java/com/edusol/DockerJenkinsApplication.java
+++ docker-jenkins/src/main/java/com/edusol/DockerJenkinsApplication.java
@@ -9,10 +9,7 @@ public class DockerJenkinsApplication {
 	public static void main(String[] args) {
 
 		System.out.println("hello");
-
-
-
-
+		System.out.println("main");
 
 		SpringApplication.run(DockerJenkinsApplication.class, args);
 	}
