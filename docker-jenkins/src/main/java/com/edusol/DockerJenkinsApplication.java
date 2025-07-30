857cccab143716e900a2a34671310ac395b49169
amit07bti <amitkumar.developer764@gmail.com>
2025-07-30 13:48:29 +0530

    updated

--- docker-jenkins/src/main/java/com/edusol/DockerJenkinsApplication.java
+++ docker-jenkins/src/main/java/com/edusol/DockerJenkinsApplication.java
@@ -7,15 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DockerJenkinsApplication {
 
 	public static void main(String[] args) {
+
 		System.out.println("hello");
 		System.out.println("hello");
 		System.out.println("fdeff");
 		System.out.println("hello");
 		System.out.println("hello");
-		System.out.println("fdeff");
-		System.out.println("hello");
-		System.out.println("hello");
-		System.out.println("fdeff");
+
 		SpringApplication.run(DockerJenkinsApplication.class, args);
 	}
 
